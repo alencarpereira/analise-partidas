4e8836be50997d4e2e75cb9d7c3c6e9977e6a6c2
alencarpereira <suporte@alencarpereira.com>
2026-05-02 13:19:46 -0300

    commit

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ function executarAnalise() {
         // ==========================
 
         // 🚀 JOGO MUITO ABERTO → OVER
-        if (totalGols >= 3.20) {
+        if (totalGols >= mediaLiga + 0.3) {
 
             let priOver = evList.find(i =>
                 i.nome === "Over 2.5" &&
